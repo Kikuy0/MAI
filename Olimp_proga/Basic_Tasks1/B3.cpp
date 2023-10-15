13aac8a05fbf1778d5dc65182291698333999e65
Kikuy0 <al6ity@gmail.com>
2023-10-15 20:52:04 +0300

    n

--- Olimp_proga/Basic_Tasks1/B3.cpp
+++ Olimp_proga/Basic_Tasks1/B3.cpp
@@ -6,17 +6,26 @@ using ll = long long;
 
 int main() {
   int n, m;
+  ios::sync_with_stdio(false);
+  cin.tie(0);
   cin >> n >> m;
-  vector<vector<ll>> a(m, vector<ll>(n));
-  vector<vector<ll>> aT(n, vector<ll>(m));
+  vector<vector<ll>> a(n, vector<ll>(m));
+  vector<vector<ll>> aT(m, vector<ll>(n));
   for (int i = 0; i < n; ++i) {
-    for (int j = 0; i < m; ++i) {
-      cin >> a[j][i];
+    for (int j = 0; j < m; ++j) {
+      cin >> a[i][j];
     }
   }
-  for (int i = 0; i <= n; ++i) {
-    for (int j = 0; j <= m; ++i) {
-      cout << a[i][j] << "\n";
+  cout << m << " " << n << "\n";
+  for (int i = 0; i < n; ++i) {
+    for (int j = 0; j < m; ++j) {
+      aT[j][i] = a[i][j];
     }
+  }
+  for (int i = 0; i < m; ++i) {
+    for (int j = 0; j < n; ++j) {
+      cout << aT[i][j] << " ";
+    }
+    cout << endl;
   }
 }
