c1e8a08701402c4ace3769768e8044866d69d348
Kikuy0 <al6ity@gmail.com>
2023-10-17 13:31:12 +0300

    task g

--- Olimp_proga/Base Of C++[1]/g.cpp
+++ Olimp_proga/Base Of C++[1]/g.cpp
@@ -6,11 +6,18 @@ using namespace std;
 int main() {
   int x1, y1, x2, y2, r1, r2;
   cin >> x1 >> y1 >> r1 >> x2 >> y2 >> r2;
-  if ((r1 + r2) <= float(sqrt(pow(x1 - x2, 2) + pow(y1 - y2, 2)))) {
-    cout << "YES"
-         << "\n";
+  float ds = sqrt(pow((x1 - x2), 2) + pow((y1 - y2), 2));
+  if ((ds < max(r1, r2))) {
+    if ((ds + min(r1, r2)) >= max(r1, r2)) {
+      cout << "YES" << endl;
+    } else {
+      cout << "NO" << endl;
+    }
   } else {
-    cout << "NO"
-         << "\n";
+    if (ds <= (r1 + r2)) {
+      cout << "YES" << endl;
+    } else {
+      cout << "NO" << endl;
+    }
   }
 }
