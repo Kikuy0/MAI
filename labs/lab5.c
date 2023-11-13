542e4ed6ed82ff720c28a0d4eb0d423bc03a9395
Kikuy0 <al6ity@gmail.com>
2023-11-14 00:44:30 +0300

    Закончил 5ю лабу

--- labs/lab5.c
+++ labs/lab5.c
@@ -1,11 +1,39 @@
 #include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-int main() {
+void make_file() {
+  FILE *file;
+  char input[100];
+
+  file = fopen("f.txt", "w");
+
+  if (file == NULL) {
+    printf("Could not create file %s\n", "f.txt");
+    exit(0);
+  }
+
+  printf("Enter strings to add to the file. Enter 'stop' to stop.\n");
+
+  while (1) {
+    fgets(input, 100, stdin);
+
+    if (strcmp(input, "stop\n") == 0) {
+      break;
+    }
+
+    fputs(input, file);
+  }
+
+  printf("File %s has been created successfully.\n", "f.txt");
+
+  fclose(file);
+}
+
+int Task() {
   FILE *f, *g;
   char ch, save;
-
   f = fopen("f.txt", "r");
   g = fopen("g.txt", "w");
 
@@ -23,7 +51,11 @@ int main() {
 
   fclose(f);
   fclose(g);
+  return 0;
+}
 
-  printf("Successfully wrote numbers from file f to file g.\n");
+int main() {
+  make_file();
+  Task();
   return 0;
 }
